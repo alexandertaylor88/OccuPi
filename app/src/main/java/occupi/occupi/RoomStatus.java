a65935957e149c1c6fad20fd6916a579fd2cc22b
alexandertaylor88 <alexanderrtaylor88@gmail.com>
2017-04-11 00:25:07 -0500

    Added link to rally page

--- app/src/main/java/occupi/occupi/RoomStatus.java
+++ app/src/main/java/occupi/occupi/RoomStatus.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class RoomStatus extends AppCompatActivity {
 
@@ -51,4 +52,10 @@ public class RoomStatus extends AppCompatActivity {
         Intent intent = new Intent (this, Rally.class);
         startActivity(intent);
     }
+
+    public void goRally(View view) {
+        Intent intent = new Intent (this, Rally.class);
+        startActivity(intent);
+    }
+
 }
