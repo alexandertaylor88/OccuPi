e77aa181a6ad28015326259c45fa210b8d87779b
Tony Klingele <ankling@siue.edu>
2017-10-19 17:50:36 -0500

    Quick import cleanup.

--- app/src/main/java/occupi/occupi/MainActivity.java
+++ app/src/main/java/occupi/occupi/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
